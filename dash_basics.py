296b7bd67a6c3ae595ddf1a30b0c425495473471
Nirmit Mishra <mishrayyhsnm2002@gmail.com>
2024-03-02 14:10:50 +0530

    formatting and turning on debug mode

--- dash_basics.py
+++ dash_basics.py
@@ -4,22 +4,25 @@ import dash
 from dash import dcc
 from dash import html
 
-adf = pd.read_csv('https://cf-courses-data.s3.us.cloud-object-storage.appdomain.cloud/IBMDeveloperSkillsNetwork-DV0101EN-SkillsNetwork/Data%20Files/airline_data.csv', 
-                            encoding = "ISO-8859-1",
-                            dtype={'Div1Airport': str, 'Div1TailNum': str, 
-                                   'Div2Airport': str, 'Div2TailNum': str})
+adf = pd.read_csv('https://cf-courses-data.s3.us.cloud-object-storage.appdomain.cloud/IBMDeveloperSkillsNetwork-DV0101EN-SkillsNetwork/Data%20Files/airline_data.csv',
+                  encoding="ISO-8859-1",
+                  dtype={'Div1Airport': str, 'Div1TailNum': str,
+                         'Div2Airport': str, 'Div2TailNum': str})
 
 data = adf.sample(n=500, random_state=42)
 
-fig = px.pie(data, values='Flights', names='DistanceGroup', title='Distance group proportion by flights')
+fig = px.pie(data, values='Flights', names='DistanceGroup',
+             title='Distance group proportion by flights')
 
 app = dash.Dash(__name__)
 
 app.layout = html.Div(children=[
-    html.H1('Airline On-time Performance Dashboard',style={'textAlign': 'center', 'color': '#503D36', 'font-size': 50}),
-    html.P('Proportion of distance group (250 mile distance interval group) by flights.', style={'textAlign':'center', 'color': '#F57241'}),
+    html.H1('Airline On-time Performance Dashboard',
+            style={'textAlign': 'center', 'color': '#503D36', 'font-size': 30}),
+    html.P('Proportion of distance group (250 mile distance interval group) by flights.',
+           style={'textAlign': 'center', 'color': '#F57241'}),
     dcc.Graph(figure=fig),
 ])
 
-if __name__=="__main__":
-    app.run_server()
+if __name__ == "__main__":
+    app.run_server(debug=True)
--- dash_interactivity_barplot.py
+++ dash_interactivity_barplot.py
@@ -13,16 +13,18 @@ from dash.dependencies import Input, Output
 
 
 # DATASET
-adf = pd.read_csv('https://cf-courses-data.s3.us.cloud-object-storage.appdomain.cloud/IBMDeveloperSkillsNetwork-DV0101EN-SkillsNetwork/Data%20Files/airline_data.csv', encoding = "ISO-8859-1", dtype={'Div1Airport': str, 'Div1TailNum': str, 'Div2Airport': str, 'Div2TailNum': str})
+adf = pd.read_csv('https://cf-courses-data.s3.us.cloud-object-storage.appdomain.cloud/IBMDeveloperSkillsNetwork-DV0101EN-SkillsNetwork/Data%20Files/airline_data.csv',
+                  encoding="ISO-8859-1", dtype={'Div1Airport': str, 'Div1TailNum': str, 'Div2Airport': str, 'Div2TailNum': str})
 
 
 # HTML
 app = dash.Dash(__name__)
 
 app.layout = html.Div(children=[
-    html.H1('Total number of flights to the destination state split by reporting airline', style={'textAlign':'center', 'color':'#503D36', 'font-size': 40}),
-    html.Div(["Input Year: ", dcc.Input(id='input-year', value='2010', type='number', style={'height':'50px', 'font-size': 35}),],
-    style={'font-size': 40}),html.Br(),html.Br(),
+    html.H1('Total number of flights to the destination state split by reporting airline', style={
+            'textAlign': 'center', 'color': '#503D36', 'font-size': 40}),
+    html.Div(["Input Year: ", dcc.Input(id='input-year', value='2010', type='number', style={'height': '50px', 'font-size': 35}),],
+             style={'font-size': 40}), html.Br(), html.Br(),
     html.Div(dcc.Graph(id='bar-plot')),
 ])
 
@@ -32,15 +34,18 @@ app.layout = html.Div(children=[
 def get_graph(entered_year):
 
     # DATA
-    df = adf[adf['Year']==int(entered_year)]
+    df = adf[adf['Year'] == int(entered_year)]
     bdf = df.groupby('DestState')['Flights'].sum().reset_index()
 
     # PLOT
-    fig = px.bar(bdf, x='DestState', y='Flights', title='Total number of flights to the destination state split by reporting airline')
-    fig.update_layout(title='Flights to Destination State', xaxis_title='Destination state', yaxis_title='Flights')
+    fig = px.bar(bdf, x='DestState', y='Flights',
+                 title='Total number of flights to the destination state split by reporting airline')
+    fig.update_layout(title='Flights to Destination State',
+                      xaxis_title='Destination state', yaxis_title='Flights')
 
     return fig
 
+
 # DRIVER
-if __name__=="__main__":
-    app.run_server()
+if __name__ == "__main__":
+    app.run_server(debug=True)
